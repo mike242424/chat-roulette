6852af7d9e8aceb41b4bc8d82ab838ad6d83dde4
Michael Byers <michael@codeandtrust.com>
2024-11-24 13:21:10 -0500

    deployement changes.

--- server/server.js
+++ server/server.js
@@ -1,86 +1,33 @@
 import { Server } from 'socket.io';
 import { PeerServer } from 'peer';
 import http from 'http';
-import cors from 'cors';
-import express from 'express';
 
-const app = express();
 const port = process.env.PORT || 3001;
-
-app.use(
-  cors({
-    origin: 'https://chat-roulette.vercel.app',
-    methods: ['GET', 'POST'],
-    credentials: true,
-  }),
-);
-
-const httpServer = http.createServer(app);
+const httpServer = http.createServer();
 
 const io = new Server(httpServer, {
   cors: {
     origin: 'https://chat-roulette.vercel.app',
     methods: ['GET', 'POST'],
-    credentials: true,
   },
+  transports: ['websocket', 'polling'],
+});
+
+io.on('connection', (socket) => {
+  console.log(`New connection: ${socket.id}`);
+
+  socket.on('peer-id', (peerId) => {
+    socket.peerId = peerId;
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Disconnected: ${socket.id}`);
+  });
 });
 
 const peerServer = PeerServer({
   server: httpServer,
   path: '/peerjs',
-  allow_discovery: true,
-});
-
-app.get('/', (req, res) => {
-  res.send('Chat Roulette server is running!');
-});
-
-const waitingQueue = [];
-const pairedUsers = new Map();
-
-io.on('connection', (socket) => {
-  socket.on('peer-id', (peerId) => {
-    socket.peerId = peerId;
-
-    if (waitingQueue.length > 0) {
-      const partnerSocket = waitingQueue.shift();
-      pairedUsers.set(socket.id, partnerSocket.id);
-      pairedUsers.set(partnerSocket.id, socket.id);
-
-      socket.emit('paired', { partnerId: partnerSocket.peerId });
-      partnerSocket.emit('paired', { partnerId: socket.peerId });
-    } else {
-      waitingQueue.push(socket);
-      socket.emit('waiting');
-    }
-  });
-
-  socket.on('message', ({ text }) => {
-    const partnerSocketId = pairedUsers.get(socket.id);
-    if (partnerSocketId) {
-      const recipient = io.sockets.sockets.get(partnerSocketId);
-      if (recipient) {
-        recipient.emit('message', { from: 'Partner', text });
-      }
-    }
-  });
-
-  socket.on('disconnect', () => {
-    const partnerId = pairedUsers.get(socket.id);
-    pairedUsers.delete(socket.id);
-
-    if (partnerId) {
-      pairedUsers.delete(partnerId);
-      const partnerSocket = io.sockets.sockets.get(partnerId);
-      if (partnerSocket) {
-        waitingQueue.push(partnerSocket);
-        partnerSocket.emit('waiting');
-      }
-    }
-
-    const index = waitingQueue.indexOf(socket);
-    if (index !== -1) waitingQueue.splice(index, 1);
-  });
 });
 
 httpServer.listen(port, () => {
