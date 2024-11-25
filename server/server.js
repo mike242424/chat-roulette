be8a54fe5966ae1ce46c245b172d04d852989484
Michael Byers <michael@codeandtrust.com>
2024-11-25 10:53:10 -0500

    12

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const io = new Server(httpServer, {
 
 // Create the PeerJS server
 const peerServer = ExpressPeerServer(httpServer, {
-  path: '/peerjs',
+  path: '/',
   allow_discovery: true,
 });
 
