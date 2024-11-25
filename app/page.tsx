80d2c426afe5d35ed3fdcc91290a44e7c5d5beea
Michael Byers <michael@codeandtrust.com>
2024-11-25 09:13:30 -0500

    5

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ const ChatRoulette = () => {
         const peer = new Peer('', {
           host: 'chat-roulette.onrender.com',
           port: 443,
-          path: '/peerjs',
+          path: '/',
           secure: true,
         });
 
