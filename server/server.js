45a2506a5f339bdfdedb99859476fa835a030ceb
Michael Byers <michael@codeandtrust.com>
2024-11-25 13:34:19 -0500

    new cors

--- server/server.js
+++ server/server.js
@@ -2,9 +2,27 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 
+import cors from 'cors';
+
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'https://chat-roulette.vercel.app',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+  },
+});
+
+app.use(
+  cors({
+    origin: 'https://chat-roulette.vercel.app',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+  }),
+);
 
 let waitingUsers = [];
 
